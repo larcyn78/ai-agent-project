10a2eb4d02363e053c5265371efb44267b7e2f17
Larcyn78 <jrasch78@gmail.com>
2025-11-20 09:43:37 -0700

    Expanded func call to all funcs

--- call_function.py
+++ call_function.py
@@ -16,8 +16,61 @@ schema_get_files_info = types.FunctionDeclaration(
     ),
 )
 
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Reads the contents of a specific file, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file name to read, relative to the working directory.",
+            ),
+        },
+    ),
+)
+
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Runs the specified python file, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The python file to execute, relative to the working directory."
+            ),
+            "args": types.Schema(
+                type=types.Type.STRING,
+                description="Optional Arguments to pass to function"
+            )
+        },
+    ),
+)
+
+schema_write_file_content = types.FunctionDeclaration(
+    name="write_file_content",
+    description="Write changes to the specified file, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file name to write to, relative to the working directory."
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="The content to write to the file"
+            )
+        },
+    ),
+)
+
 available_functions = types.Tool(
     function_declarations=[
         schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python_file,
+        schema_write_file_content
     ]
 )
